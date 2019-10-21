ee2da96f3d3cdfe333e65e49097a3fd1b01269d3
Jacqueline Cameron <jskye429@mail.com>
2019-10-21 18:35:29 -0500

    added current time and button

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 // moment stuffs 
-
 $(document).ready(function() {
+    var hourNow = moment().format('MMMM Do YYYY, h:mm:ss a')
+    $('.currentMoment').text(hourNow)
+    var currentHour = moment().format('H')
+        console.log(currentHour)
+
+$(".form-control").each(function(){
+        var someHour = $(this).attr("data-hour")
+        console.log($(this).data("hour"))
+        someHour = parseInt(someHour);
+        //+someHour
+        
+        if(someHour === currentHour){
+          $(this).css("backgroundColor","purple")
+            $(this).addClass()
+            console.log("made a match")
+       } else if(1===1){
+
+   }
+        $("button").click(function(){
+           var task = $("#comment").val()
+                var noteForMessage = "do a task"
+            $("#comment").val(noteForMessage)
+            localStorage.setItem("task 9:00",task)
+            var something = localStorage.getItem("task 9:00")
+       });
+
+    });
+
 });
-
-var hourNow = moment().format
-$('currentMoment').moment() {
-
-}
