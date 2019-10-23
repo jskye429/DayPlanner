e5b3f432d3f65ba2aeaab19d1cb82d046d195430
Jacqueline Cameron <jskye429@mail.com>
2019-10-23 17:59:17 -0500

    got local storage to save stuff via button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var hourNow = moment().format('MMMM Do YYYY, h:mm:ss a')
     $('.currentMoment').text(hourNow)
     var currentHour = moment().format('H')
+    // var currentHour = 12;
         console.log(currentHour)
 
 $(".form-control").each(function(){
@@ -12,20 +13,24 @@ $(".form-control").each(function(){
         //+someHour
         
         if(someHour === currentHour){
-          $(this).css("backgroundColor","purple")
+          $(this).css("background-color","purple")
             $(this).addClass()
             console.log("made a match")
-            }else if(1===1){
-                
-   };
-        $("button").click(function(){
+            };
+        
+            $("button").click(function(){
            var task = $("#comment").val()
-                var noteForMessage = val
-            $("#comment").val(noteForMessage)
+                // var noteForMessage = val
+            $("#comment").val(task)
             localStorage.setItem("task 9:00",task)
             var something = localStorage.getItem("task 9:00")
+            console.log("localStorage" + something)
        });
 
     });
 
 });
+
+// add local storage bit on startup of page 
+
+
